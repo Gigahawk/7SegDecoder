2f55f2d1eddb420a77085e8c6deacb5efc8cd8ea
Gigahawk <Gigahawk@users.noreply.github.com>
2016-11-13 21:06:29 -0800

    Update main.c
    Fixed bitshifts

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#define A  ((input » 3) & 1)
-#define Ap  (~((input » 3) & 1))
-#define B  ((input » 2) & 1)
-#define Bp  (~((input » 2) & 1))
-#define C  ((input » 1) & 1)
-#define Cp  (~((input » 1) & 1))
+#define A  ((input >> 3) & 1)
+#define Ap  (~((input >> 3) & 1))
+#define B  ((input >> 2) & 1)
+#define Bp  (~((input >> 2) & 1))
+#define C  ((input >> 1) & 1)
+#define Cp  (~((input >> 1) & 1))
 #define D  ((input) & 1)
 #define Dp  (~((input) & 1))
 
@@ -40,14 +40,14 @@ int main(){
 
 unsigned char getDigit(unsigned char input){
   unsigned char output,a,b,c,d,e,f,g;
-  a = ((A | C | (B&D) | (Bp&Dp)) & 1) « 6;
-  b = ((Bp | (Cp&Dp) | (C&D)) & 1) « 5;
-  c = ((Cp | D | B) & 1) « 4;
-  d = ((A | (Bp&Dp) | (Bp&C) | (C&Dp) | (B&Cp&D)) & 1) « 3;
-  e = (((Bp&Dp) | (C&Dp)) & 1) « 2;
-  f = ((A | (Cp&Dp) | (B&Cp) | (B&Dp)) & 1) « 1;
+  a = ((A | C | (B&D) | (Bp&Dp)) & 1) << 6;
+  b = ((Bp | (Cp&Dp) | (C&D)) & 1) << 5;
+  c = ((Cp | D | B) & 1) << 4;
+  d = ((A | (Bp&Dp) | (Bp&C) | (C&Dp) | (B&Cp&D)) & 1) << 3;
+  e = (((Bp&Dp) | (C&Dp)) & 1) << 2;
+  f = ((A | (Cp&Dp) | (B&Cp) | (B&Dp)) & 1) << 1;
   g = (A | (Bp&C) | (C&Dp) | (B&Cp)) & 1;
- 
-  output = (a + b + c + d + e + f + g) « 1;
+  //printf("%d,%d,%d,%d,%d,%d,%d\n", a, b, c, d, e, f, g);
+  output = (a + b + c + d + e + f + g) << 1;
   return output;
 }
