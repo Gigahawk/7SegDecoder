3c5df4d04f68f31139c4780d397ebc481c1c554c
Gigahawk <Gigahawk@users.noreply.github.com>
2016-11-14 23:58:20 -0800

    Create SevenSeg.h

---
+++ SevenSeg.h
@@ -0,0 +1,21 @@
+#ifndef SEVENSEG_H
+#define SEVENSEG_H
+
+//#define DEBUG
+
+
+
+//Simulates a decoder for a 7 segment display using K-Maps
+unsigned char getDigit(unsigned char input);
+
+//Writes a digit in the specified position
+void writeDigit(int digit, int position);
+
+//Writes a number to the display
+
+void writeNumber(int number);
+
+//Clears the display
+void clearDisplay();
+
+#endif //SEVENSEG_H
