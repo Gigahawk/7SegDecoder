e31b4b13d45cd514125e3fe33c160c5dd68fb3ed
Gigahawk <Gigahawk@users.noreply.github.com>
2016-11-14 23:53:32 -0800

    Updated main.c to SevenSeg.c
    Removed main(), added basic functions

---
+++ SevenSeg.c
@@ -0,0 +1,53 @@
+#include <DAQlib.h>
+#include "SevenSeg.h"
+
+//INPUT_Ap = 'A prime' = NOT(A) = ~INPUT_A
+#define INPUT_A	((input >> 3) & 1)
+#define INPUT_Ap	(~((input >> 3) & 1))
+#define INPUT_B	((input >> 2) & 1)
+#define INPUT_Bp	(~((input >> 2) & 1))
+#define INPUT_C	((input >> 1) & 1)
+#define INPUT_Cp	(~((input >> 1) & 1))
+#define INPUT_D	((input) & 1)
+#define INPUT_Dp	(~((input) & 1))
+
+
+unsigned char getDigit(unsigned char input){
+	unsigned char output,a,b,c,d,e,f,g;
+	a = ((INPUT_A | INPUT_C | (INPUT_B&INPUT_D) | (INPUT_Bp&INPUT_Dp)) & 1) << 6;
+	b = ((INPUT_Bp | (INPUT_Cp&INPUT_Dp) | (INPUT_C&INPUT_D)) & 1) << 5;
+	c = ((INPUT_Cp | INPUT_D | INPUT_B) & 1) << 4;
+	d = ((INPUT_A | (INPUT_Bp&INPUT_Dp) | (INPUT_Bp&INPUT_C) | (INPUT_C&INPUT_Dp) | (INPUT_B&INPUT_Cp&INPUT_D)) & 1) << 3;
+	e = (((INPUT_Bp&INPUT_Dp) | (INPUT_C&INPUT_Dp)) & 1) << 2;
+	f = ((INPUT_A | (INPUT_Cp&INPUT_Dp) | (INPUT_B&INPUT_Cp) | (INPUT_B&INPUT_Dp)) & 1) << 1;
+	g = (INPUT_A | (INPUT_Bp&INPUT_C) | (INPUT_C&INPUT_Dp) | (INPUT_B&INPUT_Cp)) & 1;
+	//printf("%d,%d,%d,%d,%d,%d,%d\n", a, b, c, d, e, f, g);
+	output = (a + b + c + d + e + f + g) << 1;
+	return output;
+}
+
+void writeDigit(int digit, int position){
+	printf("Digit is %d, got %d, writing to position %d\n", digit, getDigit(digit), position);
+	displayWrite(getDigit(digit), position);
+	return;
+}
+
+void writeNumber(int number){
+	clearDisplay();
+	int work;
+	int i = 0;
+	int divide = 1;
+	do{
+		work = ((number % (divide * 10)) / divide);
+		printf("Writing work %d to position %d\n", work, i);
+		writeDigit(work, i);
+		i++;
+		divide *= 10;
+	} while (i < 8 && work < number);
+}
+
+void clearDisplay(){
+	for (int i = 0; i < 8; i++)
+		displayWrite(0, i);
+	return;
+}
